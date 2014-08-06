12aedbf643ff05949d16d168bcefd9d8b42dcd2f
cherub0526 <cherub0526@livemail.tw>
2014-08-06 12:29:31 +0800

    add post_job

---
+++ application/views/content.php
@@ -0,0 +1,203 @@
+      <div class="row clearfix">
+        <div class="col-md-12 column">
+          <div class="tabbable" id="tabs-465105">
+            <ul class="nav nav-tabs">
+              <li class="active">
+                <a href="#panel-17473" data-toggle="tab">頁籤 1</a>
+              </li>
+              <li>
+                <a href="#panel-383009" data-toggle="tab">頁籤 2</a>
+              </li>
+            </ul>
+            <div class="tab-content">
+              <div class="tab-pane active" id="panel-17473">
+                <h1>工作分類標題</h1>
+                <table class="table">
+                  <thead>
+                    <tr>
+                      <th>
+                        #
+                      </th>
+                      <th>
+                        職位
+                      </th>
+                      <th>
+                        發布日期
+                      </th>
+                      <th>
+                        薪資水準
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>
+                        1
+                      </td>
+                      <td>
+                        TB - Monthly
+                      </td>
+                      <td>
+                        01/04/2012
+                      </td>
+                      <td>
+                        <span class="label label-default label-success">Default</span>
+
+                      </td>
+                    </tr>
+                    <tr class="active">
+                      <td>
+                        1
+                      </td>
+                      <td>
+                        TB - Monthly
+                      </td>
+                      <td>
+                        01/04/2012
+                      </td>
+                      <td>
+                        Approved
+                      </td>
+                    </tr>
+                    <tr class="success">
+                      <td>
+                        2
+                      </td>
+                      <td>
+                        TB - Monthly
+                      </td>
+                      <td>
+                        02/04/2012
+                      </td>
+                      <td>
+                        Declined
+                      </td>
+                    </tr>
+                    <tr class="warning">
+                      <td>
+                        3
+                      </td>
+                      <td>
+                        TB - Monthly
+                      </td>
+                      <td>
+                        03/04/2012
+                      </td>
+                      <td>
+                        Pending
+                      </td>
+                    </tr>
+                    <tr class="danger">
+                      <td>
+                        4
+                      </td>
+                      <td>
+                        TB - Monthly
+                      </td>
+                      <td>
+                        04/04/2012
+                      </td>
+                      <td>
+                        Call in to confirm
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+              <div class="tab-pane" id="panel-383009">
+                <table class="table">
+                  <thead>
+                    <tr>
+                      <th>
+                        #
+                      </th>
+                      <th>
+                        Product
+                      </th>
+                      <th>
+                        Payment Taken
+                      </th>
+                      <th>
+                        Status
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>
+                        1
+                      </td>
+                      <td>
+                        TB - Monthly
+                      </td>
+                      <td>
+                        01/04/2012
+                      </td>
+                      <td>
+                        Default
+                      </td>
+                    </tr>
+                    <tr class="active">
+                      <td>
+                        1
+                      </td>
+                      <td>
+                        TB - Monthly
+                      </td>
+                      <td>
+                        01/04/2012
+                      </td>
+                      <td>
+                        Approved
+                      </td>
+                    </tr>
+                    <tr class="success">
+                      <td>
+                        2
+                      </td>
+                      <td>
+                        TB - Monthly
+                      </td>
+                      <td>
+                        02/04/2012
+                      </td>
+                      <td>
+                        Declined
+                      </td>
+                    </tr>
+                    <tr class="warning">
+                      <td>
+                        3
+                      </td>
+                      <td>
+                        TB - Monthly
+                      </td>
+                      <td>
+                        03/04/2012
+                      </td>
+                      <td>
+                        Pending
+                      </td>
+                    </tr>
+                    <tr class="danger">
+                      <td>
+                        4
+                      </td>
+                      <td>
+                        TB - Monthly
+                      </td>
+                      <td>
+                        04/04/2012
+                      </td>
+                      <td>
+                        Call in to confirm
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
